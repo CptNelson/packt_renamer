41d7a0ee0a5edca26fc67dc6e30680c9724ddc96
CptNelson <toni.lappalainen@gmail.com>
2018-07-09 23:06:15 +0300

    exclude .jar

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,48 @@
 import java.io.File;
+import java.io.FilenameFilter;
 
 public class Main
 {
     final static String workingDir = System.getProperty("user.dir");
+
     public static void main(String[] args)
     {
 
         // create a File from the directory
         File directory = new File(workingDir);
 
-        // Create a File array from directory list
-        File[] listOfFiles = directory.listFiles();
+        // Create a File array from directory list and exclude .jar files
+        File[] listOfFiles = directory.listFiles(new FilenameFilter() {
+            @Override
+            public boolean accept(File file, String s) {
+                boolean result;
+                if (s.endsWith(".jar")){
+                    result = false;
+                } else {
+                    result = true;
+                }
+                return result;
+            }
+        });
+        if (listOfFiles.length <= 0) {
+            System.out.println("The folder is empty! Exiting program.");
+            return;
+        }
 
-        // Create a String array from directory list
-        String[] filesInDir = directory.list();
+
+        // Create a String array from directory list and exclude .jar files
+        String[] filesInDir = directory.list(new FilenameFilter() {
+            @Override
+            public boolean accept(File file, String s) {
+                boolean result;
+                if (s.endsWith(".jar")){
+                    result = false;
+                } else {
+                    result = true;
+                }
+                return result;
+            }
+        });
 
         // remove all non-alphabet chars from beginning of strings,
         // turn all names to lowercase and spaces to underlines.
@@ -24,24 +53,27 @@ public class Main
         //rename the files
         RenameFiles(afterUnderline, listOfFiles);
 
+        System.out.println("Done!");
+
     }
 
     private static void RenameFiles(String names[], File dir[])
     {
         for (int i = 0; i < names.length;i++) {
             dir[i].renameTo(new File(workingDir + "/" + names[i]));
+            System.out.println(names[i]);
         }
     }
 
     // Remove all non-alphabet characters from start of the strings.
     private static String[] RemoveCharsFromBeginning(String folder[])
     {
-        String[] tempFolder = folder;
+        //String[] tempFolder = folder;
         for (int i = 0; i < folder.length; i++) {
-            tempFolder[i] = folder[i].replaceFirst("[^a-zA-z]*", "");
+            folder[i] = folder[i].replaceFirst("[^a-zA-z]*", "");
         }
 
-        return tempFolder;
+        return folder;
     }
 
     private static String[] AllToLowerCase(String folder[])
